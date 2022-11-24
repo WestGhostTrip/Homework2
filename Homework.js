f4b89dfe2457e83cf578f94f2f3ff412d295174e
WestGhostTrip <98178922+WestGhostTrip@users.noreply.github.com>
2022-11-24 21:42:02 +0300

    Update Homework.js

--- Homework.js
+++ Homework.js
@@ -1,29 +1,4 @@
 "use strict";
-let solveEquation = (a, b, c) => {
-    if(a == 0)
-        return false;
-    let res = {};
-    let D = b * b - 4 * a * c;
-    console.log('D = ' + D);
-    if(D < 0)
-        return false;
-    res['discriminant'] = D;
-    if(D == 0)
-        res["quadratic roots"] = -b/(2*a);
-    else if(D > 0){
-        let tmp = [];
-        tmp.push((-b + Math.sqrt(D)) / (2 * a));
-        tmp.push((-b - Math.sqrt(D)) / (2 * a));
-        res["quadratic roots"] = tmp;
-    }
-    return res;
-}
-console.log(solveEquation(5,-6,1));
-
-
-
-
-"use strict";
 function solveEquation(a, b, c) {
     if(a == 0)
         return false;
@@ -40,6 +15,6 @@ else if(D > 0){
     tmp.push((-b - Math.sqrt(D)) / (2 * a));
 arr["quadratic roots"] = tmp;
 }   
- return arr; // array
+ return arr["quadratic roots"]; // array
 }
-console.log(solveEquation(5,-6,1));
+console.log(solveEquation(6,-6,1));
