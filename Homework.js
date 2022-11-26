52a93699999406e0f3561f48a601a291f763418f
WestGhostTrip <98178922+WestGhostTrip@users.noreply.github.com>
2022-11-26 18:57:19 +0300

    Update Homework.js

--- Homework.js
+++ Homework.js
@@ -1,20 +1,20 @@
 "use strict";
-function solveEquation(a, b, c) {
+let quadraticEquation = (a, b, c) => {
     if(a == 0)
         return false;
-let D = b * b - 4 * a * c;
-let arr = {};
-if(D < 0)
-    return false;
-arr['discriminant'] = D;
-if(D == 0)
-    arr["quadratic roots"] = -b/(2*a);
-else if(D > 0){
-    let tmp = [];
-    tmp.push((-b + Math.sqrt(D)) / (2 * a));
-    tmp.push((-b - Math.sqrt(D)) / (2 * a));
-arr["quadratic roots"] = tmp;
-}   
- return arr["quadratic roots"]; // array
+    let arr = [];
+    let D = b * b - 4 * a * c;
+    console.log('D = ' + D);
+    if(D < 0)
+        return arr;
+    if(D == 0)
+        arr["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+    else if(D > 0){
+        let tmp = [];
+        tmp.push((-b + Math.sqrt(D)) / (2 * a));
+        tmp.push((-b - Math.sqrt(D)) / (2 * a));
+        arr["quadratic roots"] = tmp;
+    }
+    return arr["quadratic roots"];
 }
-console.log(solveEquation(6,-6,1));
+console.log(quadraticEquation(-3,12,36));
